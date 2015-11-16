0f7cc06cc70c12d7c2b585dfb1feac26b09c00a5
Greg Starek <gregwsta@gmail.com>
2015-11-15 18:09:04 -0600

    Added scraping and analysis scripts

---
+++ field_value.py
@@ -0,0 +1,59 @@
+import glob
+
+
+years=["2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014"]
+
+old_abbrevs=["CRD", "TAM", "HTX", "RAM", "RAV", "RAI", "OTI", "GNB", "CLT", "NOR", "NWE", "JAX", "SDG", "KAN"]             
+
+relocated_teams={'CRD' : 'ARI', 'RAV' : 'BAL', 'GNB' : 'GB', 'HTX' : 'HOU', 'CLT' : 'IND', 'JAX' : 'JAC', 'KAN' : 'KC', 'NWE' : 'NE', 'NOR' : 'NO', 'RAI' : 'OAK', 'RAM' : 'STL', 'TAM' : 'TB', 'OTI' : 'TEN', 'SDG' : 'SD'}
+
+points={"Safety" : -2, "Field Goal" : 3, "Touchdown" : 6}
+
+#Build counters
+number_of_drives=[]
+total_score=[]
+for n in range(0,100):
+	number_of_drives.append(0)
+	total_score.append(0)
+
+
+for year in years:
+	for path in glob.glob(''.join(year + "/*drives.csv")):
+		filename=open(path, "r")
+		
+ 
+		for line in filename:
+			first_los=line.split(",")[4]
+			offense=line.split(",")[0]
+			result=line.split(",")[7].strip()
+			if first_los == "50":
+				to_go = 50
+			if first_los != "50":
+				try:
+					side_of_field=line.split(",")[4].split(" ")[0]
+					yardline = int(line.split(",")[4].split(" ")[1])
+					if side_of_field in old_abbrevs:
+						side_of_field = relocated_teams.get(side_of_field)
+				except IndexError:
+					to_go=-100
+				if side_of_field == offense:
+					to_go=(100-int(yardline))
+				if side_of_field != offense:
+					to_go=yardline
+			if first_los == "None":
+				continue
+			number_of_drives[to_go]+=1
+			if result in points.keys():
+				total_score[to_go]+=points.get(result)
+
+		
+		
+print number_of_drives
+print total_score
+
+for i in range(-101,100):
+	yard_value=float(total_score[i])/float(number_of_drives[i])
+	print i, "\t", yard_value
+	#print number_of_drives[i]
+
+
---
+++ scraping.py
@@ -0,0 +1,77 @@
+from BeautifulSoup import BeautifulSoup
+import urllib2
+
+years=["2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014"]
+
+team_dict = {'Arizona Cardinals' : 'ARI', 'Atlanta Falcons' : 'ATL', 'Baltimore Ravens' : 'BAL', 'Buffalo Bills' : 'BUF', 'Carolina Panthers' : 'CAR', 'Chicago Bears' : 'CHI', 'Cincinnati Bengals' : 'CIN', 'Cleveland Browns' : 'CLE', 'Dallas Cowboys' : 'DAL', 'Denver Broncos' : 'DEN', 'Detroit Lions' : 'DET', 'Green Bay Packers' : 'GB', 'Houston Texans' : 'HOU', 'Indianapolis Colts' : 'IND', 'Jacksonville Jaguars' : 'JAC', 'Kansas City Chiefs' : 'KC', 'Miami Dolphins' : 'MIA', 'Minnesota Vikings' : 'MIN', 'New England Patriots' : 'NE', 'New Orleans Saints' : 'NO', 'New York Giants' : 'NYG', 'New York Jets' : 'NYJ', 'Oakland Raiders' : 'OAK', 'Philadelphia Eagles' : 'PHI', 'Pittsburgh Steelers' : 'PIT', 'San Diego Chargers' : 'SD', 'San Francisco 49ers' : 'SF', 'Seattle Seahawks' : 'SEA', 'St. Louis Rams' : 'STL', 'Tampa Bay Buccaneers' : 'TB', 'Tennessee Titans' : 'TEN', 'Washington Redskins' : 'WAS'}
+
+
+base_url="http://www.pro-football-reference.com/years/"
+home_url="http://www.pro-football-reference.com"
+
+#Function to scrape drive summaries, row by row
+def get_drive_summary(team):
+    for tag1 in team:
+        try:
+            if 'Drives' in tag1.h2.text:
+                drive_title1=tag1.h2.text.split(' ')[:-1]
+                drive_info1=tag1.text
+                full_name=' '.join(word for word in drive_title1)
+                print "      Fetching", full_name 
+                team_abbrev=team_dict.get(full_name)
+                driverows=[]
+                rows = tag1.findChildren(['th', 'tr'])
+                #Cells don't print out by row, so I need an algorithm to tag each drive...
+                for row in rows:
+                    cells = row.findChildren('td')
+                    for cell in cells:
+                        value = cell.string
+                        driverows.append(value)
+
+                drive_no=0
+                quarter=1
+                time=2
+                los=3
+                duration=5
+                yards=6
+                result=7
+                
+                while result < len(driverows):
+                    with open(teamdrivesoutputfile, 'a') as fileteamdrives:
+                        fileteamdrives.write(''.join(team_abbrev + "," + str(driverows[drive_no]) + "," + str(driverows[quarter]) + "," +  str(driverows[time]) + "," + str(driverows[los]) + "," + str(driverows[duration]) + "," + str(driverows[yards]) + "," + str(driverows[result]) + "\n"))
+                        drive_no+=9
+                        quarter+=9
+                        time+=9
+                        los+=9
+                        duration+=9
+                        yards+=9
+                        result+=9
+                
+        except AttributeError:
+            continue
+
+##First, get all of the links for the box scores
+for year in years:
+    link_library=[]
+    year_url=''.join([base_url, year, "/games.htm"])
+    page=urllib2.urlopen(year_url)
+    soup=BeautifulSoup(page)
+    for a in soup.findAll('a'):
+        if 'boxscore' in a['href']:
+            if 'htm' in a['href']:
+                link_library.append(a['href'])
+    
+    ##Now iterate through each boxscore
+    for game in link_library:
+        boxscore_url=''.join([home_url, game])
+        print boxscore_url
+        game_id = boxscore_url[-16:-4]
+        page=urllib2.urlopen(boxscore_url)
+        soup=BeautifulSoup(page)
+        teamdrivesoutputfile=''.join(year + '/' + game_id + "_drives.csv")
+
+        print "    Grabbing Drive Info..."
+        team1 = soup.findAll("div", { "class" : "float_left" })
+        team2 = soup.findAll("div", { "class" : "float_left margin_left" })
+        get_drive_summary(team1)
+        get_drive_summary(team2)
